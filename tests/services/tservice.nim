19528cb6d1a479659634dcc0821b4b844d51841c
Pylgos <43234674+Pylgos@users.noreply.github.com>
2023-05-25 10:22:58 +0900

    fix tests

--- tests/services/tservice.nim
+++ tests/services/tservice.nim
@@ -1,31 +1,20 @@
-import rclnim/[init, nodes, qosprofiles, rosinterfaceimporters, subscriptions, services, clients, asyncsupports]
+import rclnim/[init, nodes, qosprofiles, rosinterfaceimporters, services, clients, asyncsupports]
 import std/[asyncdispatch, random]
 
 randomize()
 
-importInterface builtin_interfaces/msg/time
 importInterface std_srvs/srv/empty
 
 proc main =
   initRclnim()
 
   let node = newNode("my_node")
-  let sub = node.createSubscription(Time, "my_topic", SystemDefaultQoS)
   let srv = node.createService(Empty, "my_service", ServiceDefaultQoS)
   let cli = node.createClient(Empty, "my_service", ServiceDefaultQoS)
-
-  proc recvTask {.async.} =
-    for i in 0..<1000:
-      let msg = await sub.recv()
-      echo msg
-
-  proc shutdownTask {.async.} =
-    await sleepAsync 1000
-    # getGlobalContext().shutdown()
   
   proc serviceTask {.async.} =
     for i in 0..<100:
-      let (req, sender) = await srv.recv()
+      let (_, sender) = await srv.recv()
       sender.send(Empty.Response()())
       await sleepAsync rand(10)
   
@@ -33,7 +22,7 @@ proc main =
     # await sleepAsync 1000
     for i in 0..<100:
       let receiver = cli.send(Empty.Request()())
-      let resp = await receiver.recv()
+      let _ = await receiver.recv()
       await sleepAsync rand(10)
 
   proc asyncMain {.async.} =
